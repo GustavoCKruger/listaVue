9844170d8654cefe829b9fe039fbe40850d96cc4
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-03-11 14:49:09 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const cidades = ref(['São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba',
+  'Manaus',
+  'Recife',
+  'Porto Alegre',
+  'Brasília'].sort())
+
+</script>
+
+<template>
+  <ul>
+    <li v-for="(item, index) in cidades" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
